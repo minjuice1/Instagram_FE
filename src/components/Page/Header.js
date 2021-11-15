b4a7247244b8fdd4a4caa86dd6495babef952512
헹구 <hyemin085@gmail.com>
2021-11-15 21:08:57 +0900

    [추가] header 뷰잡는중

--- src/components/Page/Header.js
+++ src/components/Page/Header.js
@@ -1,16 +1,27 @@
 import React from "react";
 import "./Header.scss";
+import {message, Aftermessage, blackheart, blackhome, heart, home} from "./HeaderIcon"
+
+
 
 const Header = () => {
 
   return(
     <>
       <div className="nav">
-        <div className="logo">
+        <div className="nav_logo">
           로고
         </div>
-        <div className="nav-input">
-          <input></input>
+        <div className="nav_input">
+          <input placeholder="검색"/>
+        </div>
+        <div className="nav_icon">
+          <img src = {blackhome} alt="nav_icon"/>
+          <img src = {message} alt="nav_icon"/>
+          <img src = {blackheart} alt="nav_icon"/>
+          <img src = {blackheart} alt="nav_icon"/>
+          <img src = {heart} alt="nav_icon"/>
+          <img src = {blackheart} alt="nav_icon"/>
         </div>
       </div>
     </>
