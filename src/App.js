94c7a0b007dd053b2a754b90955dc811eb8da304
minjuice1 <minjuice93@gmail.com>
2021-11-17 01:07:29 +0900

    pull request test

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
-import "./App.scss";
+import {Routes, Route, BrowserRouter} from "react-router-dom";
+import './App.scss';
 import Home from "./components/Home/Home";
-import FindPassword from "./components/Page/FindPassword";
 import Header from "./components/Page/Header";
-import Login from "./components/Page/Login";
-import SignUp from "./components/Page/SignUp";
 
 function App() {
-	return (
-		<div className="App">
-			<BrowserRouter>
-				<Routes>
-					<Route path={"/"} element={<FindPassword />} />
-				</Routes>
-			</BrowserRouter>
-		</div>
-	);
+  return (
+    <div className="App">
+     <BrowserRouter>
+       <Header/>
+       <Routes>
+         <Route path={"/"} element={<Home/>}/>
+       </Routes>
+
+     </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
