b7c13f5e0a40283a5c2f8794a9c98c98b7da5946
헹구 <hyemin085@gmail.com>
2021-11-17 02:46:44 +0900

    [추가] 외부클릭 감지

--- src/components/Page/Header/HeaderIcon.js
+++ src/components/Page/Header/HeaderIcon.js
@@ -1,16 +1,41 @@
-import React, {useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import "./HeaderIcon.scss";
 import profile from "../../../image/profile.jpg";
 
 
 import {home, message, write, compass, heart,test, blackheart} from "../../../common/IconImage";
+
+
+
+
 const HeaderIcon = () => {
+
+  //외부클릭 감지
+  function useOutsideClick(ref) {
+
+    useEffect(() => {
+      function handleClickOutside(event) {
+        if (ref.current && !ref.current.contains(event.target)) {
+          SetLike(false);
+        }
+      }
+      document.addEventListener("mousedown", handleClickOutside);
+
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, [ref]);
+  }
+  const outsideRef = useRef(null);
+  useOutsideClick(outsideRef);
+
 
   const [like, SetLike] = useState(false);
 
   const likeClickHandler = () => {
     SetLike(!like);
   }
+
 
   if(like){
     return(
@@ -21,9 +46,33 @@ const HeaderIcon = () => {
         <img src = {compass} alt="nav_icon"/>
         <img className="nav_heart" src = {blackheart} alt="nav_icon" onClick={likeClickHandler}/>
         <img src = {test} alt="nav_icon"/>
-        <div className="like_menu">
+        <div className="like_menu" ref={outsideRef}>
+          <div className="like_today">오늘</div>
           <div className="like_cmt">
-            <div><img className="nav_profile_img" src={profile}/>헹구님이 회원님을 팔로우하기 시작했습니다. 3시간전</div>
+            <div className="like_oneCmt">
+              <div><img className="nav_profile_img" src={profile}/></div>
+              <div><a className="like_cmt_id">헹구</a>님이 회원님을 팔로우하기 시작했습니다.<a>3시간전</a>
+                <div className="like_picture">a</div>
+              </div>
+            </div>
+            <div className="like_oneCmt">
+              <div><img className="nav_profile_img" src={profile}/></div>
+              <div><a className="like_cmt_id">헹구</a>님이 회원님을 팔로우하기 시작했습니다.<a>3시간전</a>
+                <div className="like_picture">a</div>
+              </div>
+            </div>
+            <div className="like_oneCmt">
+              <div><img className="nav_profile_img" src={profile}/></div>
+              <div><a className="like_cmt_id">헹구</a>님이 회원님을 팔로우하기 시작했습니다.<a>3시간전</a>
+                <div className="like_picture">a</div>
+              </div>
+            </div>
+            <div className="like_oneCmt">
+              <div><img className="nav_profile_img" src={profile}/></div>
+              <div><a className="like_cmt_id">헹구</a>님이 회원님을 팔로우하기 시작했습니다.<a>3시간전</a>
+                <div className="like_picture">a</div>
+              </div>
+            </div>
           </div>
 
         </div>
