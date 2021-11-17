cabf4259e4bf3075f3d2f859f9a0b833920c7b65
헹구 <hyemin085@gmail.com>
2021-11-17 22:03:38 +0900

    [추가] 헤더 완성

--- src/components/Page/Header/HeaderIcon.js
+++ src/components/Page/Header/HeaderIcon.js
@@ -1,24 +1,39 @@
 import React, {useEffect, useRef, useState} from "react";
 import "./HeaderIcon.scss";
-import profile from "../../../image/profile.jpg";
+import {menu_setting, menu_save, menu_change, menu_profile} from "../../../common/IconImage";
 
 
-import {home, message, write, compass, heart,test, blackheart} from "../../../common/IconImage";
-
-
+import {home, message, write, compass, heart, test, blackheart} from "../../../common/IconImage";
+import HeaderLikeText from "./HeaderLikeText";
 
 
 const HeaderIcon = () => {
 
   const [like, SetLike] = useState(false);
   const [myProfile, SetMyProfile] = useState(false);
+
   //외부클릭 감지
   function useOutsideClick(ref) {
 
     useEffect(() => {
       function handleClickOutside(event) {
         if (ref.current && !ref.current.contains(event.target)) {
-          console.log(ref.current)
+          SetMyProfile(false);
+        }
+      }
+      document.addEventListener("mousedown", handleClickOutside);
+
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, [ref]);
+  }
+
+  function LikeOutsideClick(ref) {
+
+    useEffect(() => {
+      function handleClickOutside(event) {
+        if (ref.current && !ref.current.contains(event.target)) {
           SetLike(false);
         }
       }
@@ -29,69 +44,68 @@ const HeaderIcon = () => {
       };
     }, [ref]);
   }
-  const outsideRef = useRef(null);
-  useOutsideClick(outsideRef);
+
+  const likeSideRef = useRef(null);
+  LikeOutsideClick(likeSideRef);
+
+  const profileSideRef = useRef(null);
+  useOutsideClick(profileSideRef);
 
 
   const likeClickHandler = (event) => {
-   event.preventDefault();
+    event.preventDefault();
     SetLike(!like);
   }
 
   const myProfileClickHandler = (event) => {
     event.preventDefault();
     SetMyProfile(!myProfile);
-    console.log(myProfile);
   }
 
-  return(
+  return (
     <>
-      <img src = {home} alt="nav_icon"/>
-      <img src = {message} alt="nav_icon"/>
-      <img src = {write} alt="nav_icon"/>
-      <img src = {compass} alt="nav_icon"/>
-
-      {like? <img className="nav_heart" src = {blackheart} alt="nav_icon" onClick={likeClickHandler}/> :
-        <img className="nav_heart" src = {heart} alt="nav_icon" onClick={likeClickHandler} />}
-      {like &&
-      <div className="like_menu" ref={outsideRef}>
-        <div className="like_cmt">
-        <div className="like_today">오늘</div>
-          <div className="like_oneCmt">
-            <div><img className="nav_profile_img" src={profile}/></div>
-            <div><a className="like_cmt_id">헹구</a>님이 회원님을 팔로우하기 시작했습니다.<a>3시간전</a>
-              <div className="like_picture">a</div>
-            </div>
+      <div className="header_icon">
+        <div className="nav_icon"><img src={home} alt="nav_icon"/></div>
+        <div className="nav_icon"><img src={message} alt="nav_icon"/></div>
+        <div className="nav_icon"><img src={write} alt="nav_icon"/></div>
+        <div className="nav_icon"><img src={compass} alt="nav_icon"/></div>
+        <div className="nav_icon"><img src={message} alt="nav_icon"/></div>
+        <div className="nav_icon" ref={likeSideRef}>
+          {like ? <img className="nav_heart" src={blackheart} alt="nav_icon" onClick={likeClickHandler}/> :
+            <img className="nav_heart" src={heart} alt="nav_icon" onClick={likeClickHandler}/>}
+          {like &&
+          <div className="like_menu">
+            <HeaderLikeText />
           </div>
-          <div className="like_oneCmt">
-            <div><img className="nav_profile_img" src={profile}/></div>
-            <div><a className="like_cmt_id">헹구</a>님이 회원님을 팔로우하기 시작했습니다.<a>3시간전</a>
-              <div className="like_picture">a</div>
+          }
+        </div>
+        <div className="profile_icons" ref={profileSideRef}>
+          {myProfile ? <img className="nav_profile" src={test} alt="nav_icon"  onClick={myProfileClickHandler}/> :
+            <img src={test} alt="nav_icon"  onClick={myProfileClickHandler}/>}
+          {myProfile &&
+          <div className="myprofile">
+            <div className="profile_menu">
+              <img src={menu_profile}/>
+              <div>프로필</div>
             </div>
-          </div>
-          <div className="like_oneCmt">
-            <div><img className="nav_profile_img" src={profile}/></div>
-            <div><a className="like_cmt_id">헹구</a>님이 회원님을 팔로우하기 시작했습니다.<a>3시간전</a>
-              <div className="like_picture">a</div>
+            <div className="profile_menu">
+              <img src={menu_save}/>
+              <div>저장됨</div>
             </div>
-          </div>
-          <div className="like_oneCmt">
-            <div><img className="nav_profile_img" src={profile}/></div>
-            <div><a className="like_cmt_id">헹구</a>님이 회원님을 팔로우하기 시작했습니다.<a>3시간전</a>
-              <div className="like_picture">a</div>
+            <div className="profile_menu">
+              <img src={menu_setting}/>
+              <div>설정</div>
             </div>
-          </div>
+            <div className="profile_menu">
+              <img src={menu_change}/>
+              <div>계정전환</div>
+            </div>
+            <div className="profile_menu">
+              <div className="profile_logout">로그아웃</div>
+            </div>
+          </div>}
         </div>
       </div>
-      }
-      {myProfile? <img className="nav_profile" src = {test} alt="nav_icon" onClick={myProfileClickHandler}/>:
-        <img src = {test} alt="nav_icon" onClick={myProfileClickHandler}/>}
-      {myProfile &&
-      <div className="myprofile">
-        <div className="profile_menu">
-        <div>aaaaa</div>
-        </div>
-      </div> }
     </>
   )
 }
