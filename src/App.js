b605e2df61840344ec822bb2c3ae4ad22169f84f
minjuice1 <minjuice93@gmail.com>
2021-11-27 01:42:52 +0900

    [추가] profile 페이지 구성

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Routes, Route, BrowserRouter, Navigate} from "react-router-dom";
+import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
 import "./App.scss";
 import Home from "./components/Home/Home";
 import Header from "./components/Page/Header/Header";
@@ -9,42 +9,129 @@ import Recommendation from "./components/Page/Recommendation/Recommendation";
 import DirectMessage from "./components/Page/DirectMessage/DirectMessage";
 import AddPost from "./components/Post/PostWrite/AddPost";
 import PostDetail from "./components/Post/PostCard/PostDetail";
+import Profile from "./components/profile/Profile";
 
-import {useEffect} from "react";
-import {loginCheck} from "./redux/user/userSlice";
-import {useDispatch, useSelector} from "react-redux";
-
+import { useEffect } from "react";
+import { loginCheck } from "./redux/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
 	const dispatch = useDispatch();
-		const is_login = useSelector(state=> state.user.isLogin);
-		const token = localStorage.getItem("user");
+	const is_login = useSelector((state) => state.user.isLogin);
+	const token = localStorage.getItem("user");
 
-		useEffect(() => {
-			if(token)
-			{dispatch(loginCheck());}
-		},[token]);
-		
+	useEffect(() => {
+		if (token) {
+			dispatch(loginCheck());
+		}
+	}, [token]);
 
 	return (
-
 		<div className="App">
 			<BrowserRouter>
-				{token && <Header /> }
+				{token && <Header />}
 
 				<Routes>
-					<Route path="/login" element= {<RejectAuth redirectTo="/"> <Login /> </RejectAuth>}/>
-					<Route path="/accounts/signup" element={<RejectAuth redirectTo="/"> <SignUp /> </RejectAuth>}/>
-					<Route path ="/" element ={ <RequireAuth redirectTo="/login"> <Home /> </RequireAuth>}/>
-					<Route path ="/*" element ={ <RequireAuth redirectTo="/login"> <Home /> </RequireAuth>}/>
-					<Route path ="/postform" element ={ <RequireAuth redirectTo="/login"> <AddPost /> </RequireAuth>}/>
-					<Route path ="/message" element ={ <RequireAuth redirectTo="/login"> <DirectMessage /> </RequireAuth>}/>
-					<Route path ="/accounts/password" element ={ <RequireAuth redirectTo="/login"> <FindPassword /> </RequireAuth>}/>
-					<Route path ="/message" element ={ <RequireAuth redirectTo="/login"> <DirectMessage /> </RequireAuth>}/>
-					<Route path ="/recom" element ={ <RequireAuth redirectTo="/login"> <Recommendation /> </RequireAuth>}/>
-					<Route path ="/postdetail" element ={ <RequireAuth redirectTo="/login"> <PostDetail /> </RequireAuth>}/>
+					<Route
+						path="/login"
+						element={
+							<RejectAuth redirectTo="/">
+								{" "}
+								<Login />{" "}
+							</RejectAuth>
+						}
+					/>
+					<Route
+						path="/accounts/signup"
+						element={
+							<RejectAuth redirectTo="/">
+								{" "}
+								<SignUp />{" "}
+							</RejectAuth>
+						}
+					/>
+					<Route
+						path="/"
+						element={
+							<RequireAuth redirectTo="/login">
+								{" "}
+								<Home />{" "}
+							</RequireAuth>
+						}
+					/>
+					<Route
+						path="/*"
+						element={
+							<RequireAuth redirectTo="/login">
+								{" "}
+								<Home />{" "}
+							</RequireAuth>
+						}
+					/>
+					<Route
+						path="/postform"
+						element={
+							<RequireAuth redirectTo="/login">
+								{" "}
+								<AddPost />{" "}
+							</RequireAuth>
+						}
+					/>
+					<Route
+						path="/message"
+						element={
+							<RequireAuth redirectTo="/login">
+								{" "}
+								<DirectMessage />{" "}
+							</RequireAuth>
+						}
+					/>
+					<Route
+						path="/accounts/password"
+						element={
+							<RequireAuth redirectTo="/login">
+								{" "}
+								<FindPassword />{" "}
+							</RequireAuth>
+						}
+					/>
+					<Route
+						path="/message"
+						element={
+							<RequireAuth redirectTo="/login">
+								{" "}
+								<DirectMessage />{" "}
+							</RequireAuth>
+						}
+					/>
+					<Route
+						path="/recom"
+						element={
+							<RequireAuth redirectTo="/login">
+								{" "}
+								<Recommendation />{" "}
+							</RequireAuth>
+						}
+					/>
+					<Route
+						path="/postdetail"
+						element={
+							<RequireAuth redirectTo="/login">
+								{" "}
+								<PostDetail />{" "}
+							</RequireAuth>
+						}
+					/>
+					<Route
+						path="/profile"
+						element={
+							<RequireAuth redirectTo="/login">
+								{" "}
+								<Profile />{" "}
+							</RequireAuth>
+						}
+					/>
 				</Routes>
-
 			</BrowserRouter>
 		</div>
 	);
@@ -55,9 +142,9 @@ function RequireAuth({ children, redirectTo }) {
 	return token ? children : <Navigate to={redirectTo} />;
 }
 
-function RejectAuth({children, redirectTo}) {
+function RejectAuth({ children, redirectTo }) {
 	const token = localStorage.getItem("user");
-	return !token?  children :<Navigate to={redirectTo} />;
+	return !token ? children : <Navigate to={redirectTo} />;
 }
 
 export default App;
---
+++ src/components/profile/Profile.js
@@ -0,0 +1,112 @@
+import { React, useState } from "react";
+import "./Profile.scss";
+
+import pp from "../../image/profile.jpg";
+import { recomtest } from "../../common/IconImage";
+import { FiSettings, FiPlayCircle } from "react-icons/fi";
+import { BiBookmark } from "react-icons/bi";
+import { RiAccountBoxLine } from "react-icons/ri";
+import { MdGridOn } from "react-icons/md";
+
+const Profile = () => {
+	const [posts, SetPosts] = useState();
+	const [video, SetVideo] = useState();
+	const [saved, SetSaved] = useState();
+	const [tagged, SetTagged] = useState();
+
+	return (
+		<>
+			<div className="profile_all">
+				<div className="profile_content">
+					<div className="profile_profileBox">
+						<div className="profile_header">
+							<div className="profile_header_pp">
+								<img src={pp} alt="profile"></img>
+							</div>
+							<section className="profile_header_main">
+								<div className="profile_header_top">
+									<span>testtest</span>
+									<span>프로필 편집</span>
+
+									<FiSettings className="profile_settings" />
+								</div>
+								<ul className="profile_header_mid">
+									<span>
+										게시물 <span>10</span>
+									</span>
+									<span>
+										팔로워 <span>555</span>
+									</span>
+									<span>
+										팔로우 <span>999</span>
+									</span>
+								</ul>
+								<div className="profile_header_bottom">상태메세지</div>
+							</section>
+						</div>
+						<div className="profile_stories">
+							<div className="profile_story">
+								<div className="profile_storyBox">
+									<img src={pp} alt="profile"></img>
+								</div>
+								<div className="profile_storyName">스토리</div>
+							</div>
+							<div className="profile_story">
+								<div className="profile_storyBox">
+									<img src={pp} alt="profile"></img>
+								</div>
+								<div className="profile_storyName">스토리</div>
+							</div>
+						</div>
+						<div className="profile_post_dir">
+							<a href="/profile/posts">
+								<span className="profile_post_posts" value={posts}>
+									<MdGridOn /> 게시물
+								</span>
+							</a>
+							<a href="/profile/video">
+								<span className="profile_post_video" value={video}>
+									<FiPlayCircle /> 동영상
+								</span>
+							</a>
+							<a href="/profile/saved">
+								<span className="profile_post_saved" value={saved}>
+									<BiBookmark /> 저장됨
+								</span>
+							</a>
+							<a href="/profile/tagged">
+								<span className="profile_post_tagged" value={tagged}>
+									<RiAccountBoxLine /> 태그됨
+								</span>
+							</a>
+						</div>
+						<div className="profile_postsBox">
+							<div className="profile_postList">
+								<div className="profile_post">
+									<img src={recomtest} />
+								</div>
+								<div className="profile_post">
+									<img src={recomtest} />
+								</div>
+								<div className="profile_post">
+									<img src={recomtest} />
+								</div>
+								<div className="profile_post">
+									<img src={recomtest} />
+								</div>
+								<div className="profile_post">
+									<img src={recomtest} />
+								</div>
+								<div className="profile_post">
+									<img src={recomtest} />
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Profile;
