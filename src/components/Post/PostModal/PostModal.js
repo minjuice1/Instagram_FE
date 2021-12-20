f142ca86e528341e778f024861e4053ab3e53607
minjuice1 <minjuice93@gmail.com>
2021-12-20 16:07:57 +0900

    [추가] main에서 username 클릭 시 프로필 페이지로 이동 / 모달 삭제 기능

--- src/components/Post/PostModal/PostModal.js
+++ src/components/Post/PostModal/PostModal.js
@@ -1,17 +1,29 @@
+import { useDispatch } from 'react-redux';
+import { deletePost } from '../../../redux/post/post';
 import "./PostModal.scss";
 
-const PostModal = ({myId, writer, setOpenModal}) => {
+const PostModal = ({myId, writer, setOpenModal, postId}) => {
+
+	const dispatch = useDispatch();
 
 	const cancleClickHandler = () => {
 		setOpenModal(false);
 	};
+
+	const deleteClickHandler = () => {
+    dispatch(
+     deletePost({
+        postId,
+      }))
+		setOpenModal(false);
+  };
 
 	return (
 		<>
 			<div className="post_modal_container">
 			{myId === writer ? (
 				<div className="post_modal_myId">
-        <div>삭제</div>
+        <div onClick={deleteClickHandler}>삭제</div>
         <div>게시물로 이동</div>
         <div onClick={cancleClickHandler}>취소</div>
       </div>
