fa72ed86d48aa580455af0d7e2c988cd22b307a9
minjuice1 <minjuice93@gmail.com>
2022-01-13 17:35:18 +0900

    [merge] merge

--- src/components/Page/User/Login/SignUp.js
+++ src/components/Page/User/Login/SignUp.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef  } from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { useForm } from "react-hook-form";
